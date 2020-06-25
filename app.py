7aab49c7fdd35b1957d321194899eb8ebf1b539c
Steve Statter <statter8134@gmail.com>
2020-06-25 15:39:31 -0600

    Added endpoint to shorten URL

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Helloooo World!'
 
 
 @app.route('/super_simple')
@@ -18,6 +18,23 @@ def super_simple():
 def not_found():
     return jsonify(message='That resource was not found'), 404
 
+@app.route('/parameters')
+def parameters():
+    name = request.args.get('name')
+    age = int(request.args.get('age'))
+    if age < 18:
+        return jsonify(message="Sorry " + name + ", you are not old enough."), 401
+    else:
+        return jsonify(message="Welcome " + name + ", you are old enough!")
+
+@app.route('/url_variables/<string:name>/<int:age>') 
+def url_variables(name: str, age: int):
+    if age < 18:
+        return jsonify(message="Sorry " + name + ", you are not old enough."), 401
+    else:
+        return jsonify(message="Welcome " + name + ", you are old enough!")
+
 
 if __name__ == '__main__':
+#    app.run(debug=True)
     app.run()
