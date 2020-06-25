8058769ad10ec46d5e9fcc7e924c47ad37af7940
Steve Statter <statter8134@gmail.com>
2020-06-24 20:20:29 -0600

    add some basic status codes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 
 app = Flask(__name__)
@@ -11,7 +11,12 @@ def hello_world():
 
 @app.route('/super_simple')
 def super_simple():
-    return 'Hello from the Planetary API.'
+    return jsonify(message='Hello from the Planetary API.'), 200
+
+
+@app.route('/not_found')
+def not_found():
+    return jsonify(message='That resource was not found'), 404
 
 
 if __name__ == '__main__':
